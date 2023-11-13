66dd91750173f59f6989730ea799a9e82b5179cf
MAX POLLOCK <mcpollock00@gmail.com>
2023-11-13 14:31:57 +0000

    created links for all components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,29 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import TopBar from "./components/TopBar";
+import Home from "./Pages/Home";
+import Character from "./Pages/Character";
+import Dice from "./Pages/Dice";
+import Journal from "./Pages/Journal";
+import Profile from "./Pages/Profile";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-	return (
-		<>
-			<TopBar />
-			<NavBar />
-		</>
-	);
+  return (
+    <BrowserRouter>
+      <TopBar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/character" element={<Character />} />
+        <Route path="/dice" element={<Dice />} />
+        <Route path="/journal" element={<Journal />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+
+      <NavBar />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,17 +1,28 @@
-import book from "../images/book.png"
+import book from "../images/book.png";
 import dice from "../images/dice.png";
 import face from "../images/face.png";
 import home from "../images/home.png";
 import person from "../images/person.png";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <div className="NavBar">
-      <img src={home} alt="Home" />
-      <img src={face} alt="Character" />
-      <img src={dice} alt="Dice" id="dice" />
-      <img src={book} alt="Journal" />
-      <img src={person} alt="Profile" />
+      <Link to="/">
+        <img src={home} alt="Home" />
+      </Link>
+      <Link to="/character">
+        <img src={face} alt="Character" />
+      </Link>
+      <Link to="/dice">
+        <img src={dice} alt="Dice" id="dice" />
+      </Link>
+      <Link to="/journal">
+        <img src={book} alt="Journal" />
+      </Link>
+      <Link to="/profile">
+        <img src={person} alt="Profile" />
+      </Link>
     </div>
   );
 }
