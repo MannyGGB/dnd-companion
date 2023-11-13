043eb28d372da735ff46ce185e5314cc79230bd9
Nellie Allsop <nellie.allsop@gmail.com>
2023-11-13 13:27:13 +0000

    fix log out button

--- client/src/components/LogoutButton.jsx
+++ client/src/components/LogoutButton.jsx
@@ -1,14 +1,18 @@
-import React from "react"
-import { useAuth0 } from "@auth0/auth0-react"
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const logOut = () => {
-  const { loggingOut } = useAuth0()
+	const { logout } = useAuth0();
 
-  return (
-    <button onClick={() => loggingOut ({logoutParameters: {returnTo: window.location.orogin}})}>
-      Log out
-    </button>
-  )
-}
+	return (
+		<button
+			onClick={() =>
+				logout({ logoutParameters: { returnTo: window.location.origin } })
+			}
+		>
+			Log out
+		</button>
+	);
+};
 
-export default logOut
+export default logOut;
