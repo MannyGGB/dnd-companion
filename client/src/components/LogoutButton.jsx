eb70bc0d51ae84e9dc6e658f1c311d18f34bddef
Nellie Allsop <nellie.allsop@gmail.com>
2023-11-13 14:30:19 +0000

    push on main

--- client/src/components/LogoutButton.jsx
+++ client/src/components/LogoutButton.jsx
@@ -3,11 +3,12 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const logOut = () => {
 	const { logout } = useAuth0();
-  const { user } = useAuth0();
+	const { user } = useAuth0();
 
 	return (
 		<>
-			<img id="userPicture"
+			<img
+				id="userPicture"
 				src={user.picture}
 				alt={user.name}
 				onClick={() =>
