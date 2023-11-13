802738697fcb95bc04b08eaac3a667c162afb472
MAX POLLOCK <mcpollock00@gmail.com>
2023-11-13 16:04:57 +0000

    add changes

--- client/src/Pages/Dice.jsx
+++ client/src/Pages/Dice.jsx
@@ -1,8 +1,6 @@
+import DiceInfo from "../components/DiceInfo";
+import "../CSS/dice.css";
+
 export default function Dice() {
-  return (
-    <div>Dice</div>
-    // TopBar
-    // DiceComponent
-    // NavBar
-  )
+  return <DiceInfo />;
 }
