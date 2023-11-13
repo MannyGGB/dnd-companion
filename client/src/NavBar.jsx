2c30806cd354cc0896b49eba0a15b2bc03836bdb
MAX POLLOCK <mcpollock00@gmail.com>
2023-11-13 12:03:02 +0000

    Created Components-TopNav/BottomNavBar

---
+++ client/src/NavBar.jsx
@@ -0,0 +1,17 @@
+import book from "./images/book.png";
+import dice from "./images/dice.png";
+import face from "./images/face.png";
+import home from "./images/home.png";
+import person from "./images/person.png";
+
+export default function NavBar() {
+  return (
+    <div className="NavBar">
+      <img src={home} alt="Home" />
+      <img src={face} alt="Character" />
+      <img src={dice} alt="Dice" id="dice" />
+      <img src={book} alt="Journal" />
+      <img src={person} alt="Profile" />
+    </div>
+  );
+}
