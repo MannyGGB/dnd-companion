770a98f57f5644b5dc1cbf832cba91637bf33644
Nellie Allsop <nellie.allsop@gmail.com>
2023-11-14 12:32:39 +0000

    add notes r.e. adding character picture

---
+++ client/src/components/CharacterImage.jsx
@@ -0,0 +1,24 @@
+// export default function
+
+// import React, {useState} from "react";
+// import horns from "../Public/Cute_horns.jpeg"
+
+// function CharacterImage () {
+
+//   return
+// <img id="character-image" src={CharacterImage.imageURL} />
+// }
+
+// export default CharacterImage
+
+{
+	/* <label>
+Character Image:
+<input
+  type="text"
+  name="characterName"
+  value={characterName}
+  onChange={handleProfileChange}
+/>
+</label> */
+}
