913be9ee79e4ed2fc48a3f736a2c145b354e9cdf
Stephen <sagelvl40@gmail.com>
2023-11-15 21:31:15 +0000

    attempt at dice animation DELETE

--- client/src/components/DiceInfo.jsx
+++ client/src/components/DiceInfo.jsx
@@ -10,65 +10,66 @@ export default function Dice() {
     let totalRoll = 0;
     for (let i = 0; i < numberOfDice; i++) {
       totalRoll += Math.floor(Math.random() * sides) + 1;
+
+      setRollResult(totalRoll);
     }
-    setRollResult(totalRoll);
-  };
 
-  const handleDiceChange = (event) => {
-    setSelectedDice(parseInt(event.target.value, 10));
-  };
+    const handleDiceChange = (event) => {
+      setSelectedDice(parseInt(event.target.value, 10));
+    };
 
-  const handleRoll = () => {
-    switch (selectedDice) {
-      case 4:
-        rollDice(4);
-        break;
-      case 6:
-        rollDice(6);
-        break;
-      case 8:
-        rollDice(8);
-        break;
-      case 10:
-        rollDice(10);
-        break;
-      case 20:
-        rollDice(20);
-        break;
-      case 100:
-        rollDice(100);
-        break;
-      default:
-        rollDice(6);
-    }
-  };
+    const handleRoll = () => {
+      switch (selectedDice) {
+        case 4:
+          rollDice(4);
+          break;
+        case 6:
+          rollDice(6);
+          break;
+        case 8:
+          rollDice(8);
+          break;
+        case 10:
+          rollDice(10);
+          break;
+        case 20:
+          rollDice(20);
+          break;
+        case 100:
+          rollDice(100);
+          break;
+        default:
+          rollDice(6);
+      }
+    };
 
-  return (
-    <main>
-      <h2>Roll Different Dice</h2>
-      <div>
-        <label>Number of Dice:</label>
-        <input
-          type="number"
-          value={numberOfDice}
-          onChange={(event) => setNumberOfDice(event.target.value)}
-        />
-      </div>
-      <div>
-        <label>Select Dice Type:</label>
-        <select value={selectedDice} onChange={handleDiceChange}>
-          <option value={4}>d4</option>
-          <option value={6}>d6</option>
-          <option value={8}>d8</option>
-          <option value={10}>d10</option>
-          <option value={20}>d20</option>
-          <option value={100}>d100</option>
-        </select>
-      </div>
-      <button id="dice-button" onClick={handleRoll}>
-        Roll Dice
-      </button>
-      {rollResult !== null && <p>Result: {rollResult}</p>}
-    </main>
-  );
+    return (
+      <main>
+        <h2>Roll Different Dice</h2>
+        <div>
+          <label>Number of Dice:</label>
+          <input
+            type="number"
+            value={numberOfDice}
+            onChange={(event) => setNumberOfDice(event.target.value)}
+          />
+        </div>
+        <div>
+          <label>Select Dice Type:</label>
+          <select value={selectedDice} onChange={handleDiceChange}>
+            <option value={4}>d4</option>
+            <option value={6}>d6</option>
+            <option value={8}>d8</option>
+            <option value={10}>d10</option>
+            <option value={20}>d20</option>
+            <option value={100}>d100</option>
+          </select>
+        </div>
+        <button id="dice-button" onClick={handleRoll}>
+          Roll Dice
+        </button>
+        {rollResult !== null && <p>Result: {rollResult}</p>}
+      </main>
+    );
+  };
 }
