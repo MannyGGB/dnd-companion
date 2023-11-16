d42b8113f581d692dcf645c2d269a9aba98040b6
Nellie Allsop <nellie.allsop@gmail.com>
2023-11-16 10:32:37 +0000

    add auth0 to character form

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -5,25 +5,25 @@ import { useAuth0 } from "@auth0/auth0-react";
 import "../CSS/profile.css";
 
 export default function Profile({ API_Url }) {
-  const { user, isAuthenticated } = useAuth0();
+	const { user, isAuthenticated } = useAuth0();
 
-  return (
-    <main>
-      {isAuthenticated && (
-        <div className="profile">
-          <img className="profilePicture" src={user.picture} alt={user.name} />
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
-          <LogoutButton />
-        </div>
-      )}
-      {!isAuthenticated && (
-        <div className="profile">
-          <p>Log in to your account to save and update your character!</p>
-          <LoginButton />
-        </div>
-      )}
-    </main>
-    // ProfilePage
-  );
+	return (
+		<main>
+			{isAuthenticated && (
+				<div className="profile">
+					<img className="profilePicture" src={user.picture} alt={user.name} />
+					<h2>{user.name}</h2>
+					<p>{user.email}</p>
+					<LogoutButton />
+				</div>
+			)}
+			{!isAuthenticated && (
+				<div className="profile">
+					<p>Log in to your account to save and update your character!</p>
+					<LoginButton />
+				</div>
+			)}
+		</main>
+		// ProfilePage
+	);
 }
