d1978234bbb639c9bcf6fc1407f0d4090934552e
MAX POLLOCK <mcpollock00@gmail.com>
2023-11-16 09:01:44 +0000

    tweaked letter on title

--- client/src/components/TopBar.jsx
+++ client/src/components/TopBar.jsx
@@ -7,7 +7,7 @@ export default function TopBar() {
     <div className="TopBar">
       <Link id="top-bar-link" to="/">
         <img src={logo} alt="Logo" id="logo" />
-        <h4 id="app-title">Dnd Companion</h4>
+        <h4 id="app-title">DnD Companion</h4>
       </Link>
     </div>
   );
